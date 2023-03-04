ead098a349b14aa67aa83253176b24adc09fbb45
JPEhl <jp.ehlenberger@icloud.com>
2023-03-04 13:19:32 +0100

    Anpassungen (vereinfacht)

--- 03 Bedingte Verzweigungen/02_wetterwarnung.py
+++ 03 Bedingte Verzweigungen/02_wetterwarnung.py
@@ -1,5 +1,3 @@
 """
 c) Implementiere ein Programm, das eine Glatteiswarnung ausgibt, wenn es zu kalt ist und ansonsten ausgibt, dass keine Gefahr besteht.
 """
-
-temperatur = int(input("Wie viel Grad sind es aktuell: "))
